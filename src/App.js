92260e6e559a1fcf5838fc438314e9c97f5611c8
returnaction <obergannv@gmail.com>
2023-12-15 02:06:54 -0500

    done

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import "bootstrap/dist/css/bootstrap.css";
 const url =
   "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
+const getNewColor = () => {
+  const red = Math.floor(Math.random() * 128);
+  const green = Math.floor(Math.random() * 128);
+  const blue = Math.floor(Math.random() * 128);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+
 function App() {
   const [quotesArr, setQuotesArr] = useState([]);
   const [quote, setQuote] = useState({
     quote: "Code or not to code?",
     author: "CwLine",
   });
+  const [color, setColor] = useState(getNewColor());
 
   const getQuote = () => {
     let randomNumber = Math.floor(Math.random() * quotesArr.length);
     setQuote(quotesArr[randomNumber]);
+    setColor(getNewColor());
   };
 
   const fetchQuotes = async () => {
@@ -29,20 +39,26 @@ function App() {
 
   useEffect(() => {
     fetchQuotes();
+    getNewColor();
   }, []);
 
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ backgroundColor: color, transition: "all 1s" }}
+    >
       <div id="wrapper">
         <div id="quote-box">
-          <div className="quote-text">
+          <div className="quote-text" style={{ color: color }}>
             <i class="fa fa-quote-left" />
             <span id="text">{quote.quote}</span>
           </div>
           <div className="quote-author">
             {" - "}
 
-            <span id="author">{quote.author}</span>
+            <span id="author" style={{ color: color }}>
+              {quote.author}
+            </span>
           </div>
 
           <div className="buttons">
@@ -54,9 +70,13 @@ function App() {
               )}
               id="tweet-quote"
             >
-              <i className="fa fa-twitter-square"></i>
+              <i
+                style={{ backgroundColor: color }}
+                className="fa fa-twitter"
+              ></i>
             </a>
             <button
+              style={{ backgroundColor: color }}
               id="new-quote"
               className="button"
               onClick={() => getQuote()}
